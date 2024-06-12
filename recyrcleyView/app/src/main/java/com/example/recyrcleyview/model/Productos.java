e86c969f5b06592d970059ca3e9908f273faf71a
Rodri Quelali <rodriquelali@gmail.com>
2024-06-12 18:49:18 -0400

    recrcleyView parte 1

---
+++ recyrcleyView/app/src/main/java/com/example/recyrcleyview/model/Productos.java
@@ -0,0 +1,37 @@
+package com.example.recyrcleyview.model;
+
+public class Productos {
+    private String nombre;
+    private String descripcion;
+    private int precio;
+
+    public Productos(String nombre, String descripcion, int precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+}
