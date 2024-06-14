9dcca640baa43548c8ca00b4bfb1ed3713324ac4
Rodri Quelali <rodriquelali@gmail.com>
2024-06-14 19:44:35 -0400

    recrcleyView parte 2

--- recyrcleyView/app/src/main/java/com/example/recyrcleyview/MainActivity.java
+++ recyrcleyView/app/src/main/java/com/example/recyrcleyview/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.recyrcleyview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.recyrcleyview.adapter.ListaAdapterProducto;
+import com.example.recyrcleyview.model.Productos;
 
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView listaRecyrcleyViewPersona;
+    ArrayList<Productos> listaProductos = new ArrayList<Productos>();
+
+    ListaAdapterProducto listaAdapterProducto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cargarR();
+        cargadoListaProducto();
+    }
+
+    public void cargarR(){
+        listaRecyrcleyViewPersona = (RecyclerView) findViewById(R.id.listaRPersona);
+    }
+
+    public void cargadoListaProducto(){
+        listaRecyrcleyViewPersona.setLayoutManager(new LinearLayoutManager(this));
+
+        listaProductos.add(new Productos("laptop","computadora de escritorio", 1200));
+        listaProductos.add(new Productos("laptop escritorio","computadora de escritorio", 1200));
+        listaAdapterProducto = new ListaAdapterProducto(listaProductos);
+        listaRecyrcleyViewPersona.setAdapter(listaAdapterProducto);
+
     }
 }
