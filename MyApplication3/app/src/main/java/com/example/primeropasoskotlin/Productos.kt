9b5057b89ee36f99488c4071bc352d9ef13d78dc
rodriquelali <rodriquelali@gmail.com>
2024-08-28 21:42:10 -0400

    correccion de conflicto

--- MyApplication3/app/src/main/java/com/example/primeropasoskotlin/Productos.kt
+++ MyApplication3/app/src/main/java/com/example/primeropasoskotlin/Productos.kt
@@ -19,7 +19,6 @@ class Productos : AppCompatActivity() {
     lateinit var txtNombre: EditText
     lateinit var txtPrecio: EditText
     lateinit var txtCodigo: EditText
-class Productos : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
